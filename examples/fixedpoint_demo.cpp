03702f1bfd748f825d8190ae37c0389304b2702a
Jarosław Pelczar <jarek@jpelczar.com>
2026-09-13 12:23:53 +0200

    Add fixed point class support
    Signed-off-by: Jarosław Pelczar <jarek@jpelczar.com>

---
+++ examples/fixedpoint_demo.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <microfmt/fixed_point.hpp>
+#include <microfmt/microfmt.hpp>
+
+int main() {
+  int32_t vcc_mv = 3295;
+  int32_t current_ua = 14200;
+  int16_t subzero_temp_cdeg = -45; // -0.45 C
+
+  auto out = microfmt::format<128>(
+      "VCC: {} V, Current: {} mA, Temp: {} C",
+      microfmt::fixed<1000, 2>(vcc_mv),          // "3.29"
+      microfmt::fixed<1000, 3>(current_ua),      // "14.200"
+      microfmt::fixed<100, 2>(subzero_temp_cdeg) // "-0.45"
+  );
+
+  std::fwrite(out.view().data(), 1, out.size(), stdout);
+  std::putchar('\n');
+}
